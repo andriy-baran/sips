324daa592c23af2fac58eb471e112627ef9df336
Orest Kostiuk <xxmolekula@gmail.com>
2018-12-06 22:43:41 +0200

    change model name  to Account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:full_name, :phone, :email, :password, :password_confirmation, :avatar, :rate_per_hour)
+    params.require(:account).permit(:full_name, :phone, :email, :password, :password_confirmation, :avatar, :rate_per_hour)
   end
 
   def account_update_params
-    params.require(:user).permit(:full_name, :phone, :email, :password, :password_confirmation, :avatar, :rate_per_hour)
+    params.require(:account).permit(:full_name, :phone, :email, :password, :password_confirmation, :avatar, :rate_per_hour)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,32 @@
 
 ActiveRecord::Schema.define(version: 2018_12_05_201059) do
 
+  create_table "accounts", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.string "phone"
+    t.float "rate_per_hour"
+    t.string "avatar"
+    t.index ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
   create_table "cashboxes", force: :cascade do |t|
     t.integer "pos_id"
     t.integer "product_id"
@@ -53,32 +79,6 @@ ActiveRecord::Schema.define(version: 2018_12_05_201059) do
     t.datetime "updated_at", null: false
     t.index ["pos_id"], name: "index_stocks_on_pos_id"
     t.index ["product_id"], name: "index_stocks_on_product_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "full_name"
-    t.string "phone"
-    t.float "rate_per_hour"
-    t.string "avatar"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "variants", force: :cascade do |t|
