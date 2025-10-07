b8cf481e4e09cacc25e518e7cb549cc32de19d78
Andrii Baran <andriy.baran.v@gmail.com>
2025-10-07 10:48:29 +0300

    Closes alert messages with Bootstrap
    Updates alert closing logic to use Bootstrap's JavaScript API.
    This ensures proper handling of alert dismissal and prevents potential
    JavaScript errors.

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,6 +1,6 @@
 import '~/index.css'
 import $ from 'jquery'
-import 'bootstrap'
+import * as bootstrap from 'bootstrap'
 import 'jquery-ui-dist/jquery-ui';
 import Rails from '@rails/ujs'
 import * as Turbolinks from 'turbolinks'
@@ -18,7 +18,10 @@ Turbolinks.start()
 
 document.addEventListener('turbolinks:load', function() {
   setTimeout(function() {
-    $(".alert").alert('close');
+    if (document.querySelector('.alert')) {
+      var alert = bootstrap.Alert.getOrCreateInstance('.alert')
+      alert.close()
+    }
   }, 3000);
 });
 
